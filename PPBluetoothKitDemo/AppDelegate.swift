5eaa4f851b1a167e7b62d7f2f048320308526ee6
JsonT <tuowei@lefu.cc>
2024-07-24 15:13:40 +0800

    Unified management trial version "AppKey" and "AppSecrect"

--- PPBluetoothKitDemo/AppDelegate.swift
+++ PPBluetoothKitDemo/AppDelegate.swift
@@ -8,9 +8,7 @@
 import UIKit
 import PPBluetoothKit
 
-// Please be sure to replace it with your AppKey and AppSecrect
-let DemoAppKey = "lefub60060202a15ac8a"
-let DemoAppSecrect = "UCzWzna/eazehXaz8kKAC6WVfcL25nIPYlV9fXYzqDM="
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
---
+++ PPBluetoothKitDemo/Classes/Tools/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  PPBluetoothKitDemo
+//
+//  Created by lefu on 2024/7/24
+//  
+
+
+import Foundation
+
+// Please be sure to replace it with your AppKey and AppSecrect
+let DemoAppKey = "lefub60060202a15ac8a"
+let DemoAppSecrect = "UCzWzna/eazehXaz8kKAC6WVfcL25nIPYlV9fXYzqDM="
