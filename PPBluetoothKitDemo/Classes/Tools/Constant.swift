88e56d2373037880711ed38bae2b103b69322451
tuowei <tuowei@lefu.cc>
2025-09-15 18:44:38 +0800

    feat | 添加计算库secret注释

--- PPBluetoothKitDemo/Classes/Tools/Constant.swift
+++ PPBluetoothKitDemo/Classes/Tools/Constant.swift
@@ -21,8 +21,13 @@ let DemoAppSecrect = "UCzWzna/eazehXaz8kKAC6WVfcL25nIPYlV9fXYzqDM="
 // 请务必将下面的计算库 Secret 换成你们自己的，否则可能无法计算出结果。请联系商务获取计算库密钥
 // Please be sure to replace the following calculation library Secret with your own, otherwise it may not be possible to calculate the result.Please contact the business department to obtain the calculation library key
 
+/// TwoLegs140   5.0.5-W
 let DemoSecretAlternate4Common = "Hl3R4f7lyP+neAWbZBmvW2di1+gZXGzuYNZ0kAwAjnElNrR/CaoAcy1mB2CfHizc"
+/// TwoLegs140  多版本共存Bh1
 let DemoSecretAlternate4_0 = "up5dkj8oEi4NUmcziVCUkaAZM4j80ffS0p8yEkDNUP+5lY4izQ7iTMV+8Qg4R/Lg"
+/// TwoLegs240  多版本共存Bh3
 let DemoSecretAlternate4_1 = "9emloLinGXEk1n8oqsccPkFjEU4mGOK6r69Lgk2XDBcnQM19AwnWOw/9lIID0ove"
+/// 1.7.2   多版本共存Bh2
 let DemoSecretAlternate8 = "i+OS1Ir4pHlQEnN+N0NGAigejDX3ztNAhHOCNIUXA+TNUHCD5aOKhGVE4jf3uKpi"
+/// Body270 多版本共存Bh5
 let DemoSecretAlternate8Common = "Zvpw7/fttSdetsb9CBPEdsKTIf1Dr7Avu9AnOm8o9LdH3bAxwUeOFJaW/OqmEldp"
